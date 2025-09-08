38eb211619a1c25d554f10eaaa4f5b9a757cfa00
Talha-software <talha.sajjad.software@gmail.com>
2025-09-08 16:10:36 +0500

    added html template

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 Route::get('/dashboard', [userController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
